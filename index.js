b6c5fcbded0b4bd6dd0fa3bf5e73939f695db866
nicola-laurel-willis <124948483+nicola-laurel-willis@users.noreply.github.com>
2023-04-08 23:32:58 +0100

    made a start - creating a simpler SVG from scratch to see if I can generate them via javascript

--- index.js
+++ index.js
@@ -35,22 +35,22 @@ const zoomOut = zoom(increaseViewBoxDimension)
 
 
 window.addEventListener("load", () => {
-  const starsSvgElement = getSvgElement("stars-svg-object", "stars-svg-element")
-  const zoomInButton = document.getElementById("zoom-in")
-  const resetButton = document.getElementById("reset")
-  const zoomOutButton = document.getElementById("zoom-out")
+  // const starsSvgElement = getSvgElement("stars-svg-object", "stars-svg-element")
+  // const zoomInButton = document.getElementById("zoom-in")
+  // const resetButton = document.getElementById("reset")
+  // const zoomOutButton = document.getElementById("zoom-out")
 
-  zoomInButton.addEventListener("click", () => {
-    zoomIn(starsSvgElement)
-  })
+  // zoomInButton.addEventListener("click", () => {
+  //   zoomIn(starsSvgElement)
+  // })
   
-  zoomOutButton.addEventListener("click", () => {
-    zoomOut(starsSvgElement)
-  })
+  // zoomOutButton.addEventListener("click", () => {
+  //   zoomOut(starsSvgElement)
+  // })
 
-  resetButton.addEventListener("click", () => {
-    starsSvgElement.setAttribute("viewBox", `0 0 ${basicSvgDimension} ${basicSvgDimension}`)
-  })
+  // resetButton.addEventListener("click", () => {
+  //   starsSvgElement.setAttribute("viewBox", `0 0 ${basicSvgDimension} ${basicSvgDimension}`)
+  // })
 
 })
 
@@ -59,4 +59,52 @@ window.addEventListener("load", () => {
 
 
 
+// utility functions
+const getMultiplicationAmount = unit => {
+  if(unit === "kb"){
+    return 0
+  }
+  if(unit === "mb"){
+    return 1000
+  }
+  if(unit === "gb"){
+    return 1000000
+  }
+}
+//------------------------------------
 
+// const windowWidth = window.innerWidth
+// const windowHeight = window.innerHeight
+// console.log("windowWidth", windowWidth)
+// console.log("windowHeight", windowHeight)
+
+const numericalSizeInputElement = document.getElementById("numerical-size")
+const unitInputElement = document.getElementById("unit")
+
+const visualiseButton = document.getElementById("visualise-button")
+
+visualiseButton.addEventListener("click", () => {
+
+  const numericalSize = numericalSizeInputElement.value
+  const unit = unitInputElement.value
+  console.log("numerical size: ", numericalSize, "unit: ", unit)
+
+  const multiplicationAmount = getMultiplicationAmount(unit)
+
+  const sizeInKilobytes = numericalSize * multiplicationAmount
+
+
+  console.log("sizeInKilobytes", `${sizeInKilobytes}kb`)
+
+  if(sizeInKilobytes <= 1000000){
+    const pixelVersionSquare = sizeInKilobytes/2
+    const squareSide = Math.sqrt(pixelVersionSquare)
+  
+    const visualised = document.getElementById("visualised")
+    visualised.setAttribute("style", `width: ${squareSide}px; height: ${squareSide}px`)
+  }
+  else {
+    console.log("ERROR: too big")
+  }
+
+})
